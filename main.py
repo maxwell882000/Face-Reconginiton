0c249489f42abb46d205e82d12e2fa239a30d14a
m_axwel_l <shaxa882@gmail.com>
2023-04-10 10:20:08 +0500

    add multiple detection

--- main.py
+++ main.py
@@ -24,19 +24,51 @@ class Body(BaseModel):
     unknown: str
 
 
+class BodyMultiple(BaseModel):
+    compare: list
+    unknown: str
+
+
+@app.post("/model/image-comparison/multiple")
+async def comparison(data: BodyMultiple):
+    file_like = BytesIO(base64.b64decode(
+        data.unknown.replace("data:image/octet-stream;base64,", "")))
+    unknown_face = face_recognition.load_image_file(file_like)
+    try:
+        # map to face encodings ,then take only images that have faces, take first face
+        print(data.compare)
+        compare_encoding = list(map(lambda x: x[0],
+                                    filter(lambda x: len(x) > 0,
+                                           list(
+                                        map(lambda x: face_recognition.face_encodings(face_recognition.load_image_file(x)),
+                                            data.compare)
+                                    )
+        )
+        )
+        )
+        unknown_encoding = face_recognition.face_encodings(unknown_face)
+    except IndexError:
+        return {"detected": False, "status": "Error"}
+    for items in compare_encoding:
+        if face_recognition.compare_faces([items], unknown_encoding[0])[0]:
+            return {"detected" : 1}
+    return {"detected": 0}
+
+
 @app.post("/model/image-comparison")
 async def root(data: Body):
     # return {"detected" : True}
     async with httpx.AsyncClient() as client:
         object = await client.get(data.compare)
-    file_like = BytesIO(base64.b64decode(data.unknown.replace("data:image/octet-stream;base64,", "")))
+    file_like = BytesIO(base64.b64decode(
+        data.unknown.replace("data:image/octet-stream;base64,", "")))
     compare_face = face_recognition.load_image_file(BytesIO(object.content))
     unknown_face = face_recognition.load_image_file(file_like)
     try:
         compare_encoding = face_recognition.face_encodings(compare_face)[0]
         unknown_encoding = face_recognition.face_encodings(unknown_face)
-        if len(unknown_encoding) > 1 or len(unknown_encoding) == 0: 
-            return {"detected" : False}
+        if len(unknown_encoding) > 1 or len(unknown_encoding) == 0:
+            return {"detected": False}
     except IndexError:
         return {"detected": False, "status": "Error"}
     e = face_recognition.compare_faces([compare_encoding], unknown_encoding[0])
